67299982cbbed6b642285fb4b3be28053fe4ef57
High <bigbig5661@gmail.com>
2026-08-29 16:34:08 +0700

    Update Esp.lua

--- assets/Esp.lua
+++ assets/Esp.lua
@@ -308,7 +308,7 @@ function Library:UpdateChams(Player, Data)
 
 		if IsHead then
 		    adorn.Height = Size.Y + 0.35
-		    adorn.Radius = (Size.X / 2) + (IsOutline and 0.06 or 0.02)
+		    adorn.Radius = (Size.X / 2) + (IsOutline and 0.15 or 0.05)
 		else
 		    adorn.Size = Size + (IsOutline and OutlineOffset or InlineOffset)
 		end
