7450597bf13bbe8e56d61eb8d2436d0fbc45b7b3
High <bigbig5661@gmail.com>
2026-08-30 20:07:56 +0700

    Update Esp library.lua

--- assets/Esp library.lua
+++ assets/Esp library.lua
@@ -1677,11 +1677,11 @@ function Library:Update(Player, Data)
                 })
                 Objects.OOVHealthOutline.Visible = true
 
-                if OOV.ShowHealthText then
+                if OOV.Text then
                     Objects.OOVHealthText.Text = Format("%d", Floor(Health))
                     Objects.OOVHealthText.TextTransparency = 1 - Alpha
                     Objects.OOVHealthText.AnchorPoint = NewVector2(1, 0.5)
-                    Objects.OOVHealthText.Position = DimOffset(CenterX - Size * 0.62 - 9, CenterY + (BarH * 0.5) - (BarH * Ratio) + 0.5)
+                    Objects.OOVHealthText.Position = DimOffset(CenterX - Size * 0.62 - 9, CenterY + (BarH * 0.5) - (BarH * Ratio))
                     Objects.OOVHealthText.Visible = true
                 else
                     Objects.OOVHealthText.Visible = false
