913ff68bcbacdc50b72fc99a81712cca52f338af
High <bigbig5661@gmail.com>
2026-09-18 22:01:38 +0700

    Update Gamesense example.lua

--- assets/Gamesense example.lua
+++ assets/Gamesense example.lua
@@ -1,6 +1,8 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/leanandhigh/Leanhighasset/refs/heads/main/assets/Gamesense.lua"))()
 local Players = game:GetService("Players")
+
 local Window = Library:Window({CloseBind = Enum.KeyCode.Home})
+
 local Rage = Window:CreateTab({Icon = "rbxassetid://18248771514"})
 local AntiAim = Window:CreateTab({Icon = "rbxassetid://15453313321"})
 local Aimbot = Window:CreateTab({Icon = "rbxassetid://15453335745"})
@@ -10,10 +12,39 @@ local Weapons = Window:CreateTab({Icon = "rbxassetid://15453354931"})
 local PlayerList = Window:CreateTab({Icon = "rbxassetid://15453359751"})
 local Configs = Window:CreateTab({Icon = "rbxassetid://15453364412"})
 local Lua = Window:CreateTab({Icon = "rbxassetid://18240049800"})
+
 local ActualPlayerList
 Window:SetTab(8)
+
+-- Cosmetics changer (left of main UI)
+local SkinChanger = Window:CreateSkinChanger({
+    Name = "Cosmetics Changer",
+    Width = 300,
+    Columns = 4,
+    CellSize = 64,
+    Flag = "SelectedSkin",
+    Callback = function(SkinName, Data, Weapon)
+        print("Selected:", SkinName, "for", Weapon)
+        -- apply skin here
+    end,
+})
+
+SkinChanger:AddWeapon("Assault Rifle", {Image = "rbxassetid://12206409737"})
+SkinChanger:AddWeapon("Burst Rifle", {Image = "rbxassetid://12206409737"})
+SkinChanger:AddWeapon("Shotgun", {Image = "rbxassetid://12206409737"})
+SkinChanger:AddWeapon("Handgun", {Image = "rbxassetid://12206409737"})
+
+SkinChanger:Add("Default", {Image = "rbxassetid://12206409737", Weapon = "Assault Rifle"})
+SkinChanger:Add("AK-47", {Image = "rbxassetid://12206409737", Weapon = "Assault Rifle"})
+SkinChanger:Add("AUG", {Image = "rbxassetid://12206409737", Weapon = "Assault Rifle"})
+SkinChanger:Add("Aqua Burst", {Image = "rbxassetid://12206409737", Weapon = "Burst Rifle"})
+SkinChanger:Add("Pixel Burst", {Image = "rbxassetid://12206409737", Weapon = "Burst Rifle"})
+SkinChanger:Add("Default SG", {Image = "rbxassetid://12206409737", Weapon = "Shotgun"})
+SkinChanger:Add("Default HG", {Image = "rbxassetid://12206409737", Weapon = "Handgun"})
+
 AntiAim:Section({Fill = true})
 AntiAim:Section({Fill = true, Side = "Right"})
+
 do -- Rage
     Rage:ImageDropdown({
         Name = "Weapon type",
@@ -51,6 +82,7 @@ do -- Rage
     RageSection:Label()
     RageSection:MultiBox({Content = {"Option 1", "Option 2"}})
 end
+
 do -- Visuals
     local VisualsSubSection, VisualsSubSection2, VisualsSubSection3, VisualsSubSection4 = Visuals:SubSection({
         Name = "Category",
@@ -88,6 +120,7 @@ do -- Visuals
         Disable = {"Disabled", 0, 11}
     })
 end
+
 do -- Settings
     local SettingsSection = Settings:Section({Name = "Settings", Side = "Right", Fill = true})
     SettingsSection:Label({Message = "Menu key"}):Keybind({
@@ -146,13 +179,20 @@ do -- Settings
             Library:ToggleSpectatorList(State)
         end
     })
-    Library:AddSpectator(Player)
-    Library:RemoveSpectator(Player)
-    Library:ClearSpectators()
-    Library:ToggleSpectatorList(true)
+    SettingsSection:Toggle({
+        Name = "Skin Changer",
+        Default = true,
+        Flag = "ShowSkinChanger",
+        Callback = function(State)
+            if SkinChanger then
+                SkinChanger:SetVisibility(State)
+            end
+        end
+    })
     SettingsSection:Button({Name = "Unload", Callback = Library.Unload})
     SettingsSection:Button({Name = "Disable all", Callback = Library.Disable})
 end
+
 do -- Weapons
     local SkinsSection = Weapons:Section({Name = "Skins", Fill = true})
     local SkinList = SkinsSection:List({Size = 200})
@@ -168,19 +208,8 @@ do -- Weapons
         Size = UDim2.fromOffset(5, 5),
         Position = UDim2.new(0, 11, 0.5, 0)
     })
-    SkinList:AddValue("Test Skin 3", {
-        Image = "http://www.roblox.com/asset/?id=12206409737",
-        Color = Color3.fromRGB(198, 7, 232),
-        Size = UDim2.fromOffset(5, 5),
-        Position = UDim2.new(0, 11, 0.5, 0)
-    })
-    SkinList:AddValue("Test Skin 4", {
-        Image = "http://www.roblox.com/asset/?id=12206409737",
-        Color = Color3.fromRGB(36, 232, 1),
-        Size = UDim2.fromOffset(5, 5),
-        Position = UDim2.new(0, 11, 0.5, 0)
-    })
 end
+
 do -- Aimbot
     local AimbotSubSection, AimbotSubSection2 = Aimbot:SubSection({
         Name = "Category",
@@ -193,6 +222,7 @@ do -- Aimbot
         }
     })
 end
+
 do -- PlayerList
     local PlayerSection = PlayerList:Section({Name = "Players", Fill = true})
     local PlayerAdjustments = PlayerList:Section({Name = "Adjustments", Fill = true, Side = "Right"})
@@ -210,6 +240,7 @@ do -- PlayerList
     end
     PlayerAdjustments:Toggle({Name = "Whitelisted"})
 end
+
 do -- Configs
     local ConfigSection = Configs:Section({Name = "Configs", Fill = true})
     local LuaSection = Configs:Section({Name = "LUA", Side = "Right", Fill = true})
@@ -242,10 +273,12 @@ do -- Configs
     LuaSection:Button({Name = "Unload script"})
     LuaSection:Button({Name = "Refresh list"})
 end
+
 do -- Lua
     local TabA = Lua:Section({Name = "Tab A", Fill = true})
     local TabB = Lua:Section({Name = "Tab B", Side = "Right", Fill = true})
 end
+
 do -- Connections
     Library:Connection(Players.PlayerAdded, function(Player)
         if not ActualPlayerList then return end
@@ -260,4 +293,5 @@ do -- Connections
         })
     end)
 end
+
 Library:Init()
